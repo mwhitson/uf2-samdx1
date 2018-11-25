7ed4148276b1ce1aba115f0909c698f70e4f11b1
mwhitson <mwhitson@users.noreply.github.com>
2018-11-24 23:13:47 -0500

    aj-snowflake tweaks for UF2 bootloader

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-BOARD=zero
+BOARD=aj-snowflake
 -include Makefile.user
 include boards/$(BOARD)/board.mk
 CC=arm-none-eabi-gcc
---
+++ boards/aj-snowflake/board_config.h
@@ -0,0 +1,16 @@
+#ifndef BOARD_CONFIG_H
+#define BOARD_CONFIG_H
+
+#define VENDOR_NAME "Whitson Micro"
+#define PRODUCT_NAME "Special Snowflake"
+#define VOLUME_LABEL "SNOWFLAKE"
+#define INDEX_URL "https://github.com/mwhitson/aj-snowflake"
+#define BOARD_ID "SAMD21G18A-AJsnowflake-r2"
+
+/* Piggybacked from Adafruit Feather M0 */
+#define USB_VID 0x239A
+#define USB_PID 0x0015
+
+#define LED_PIN PIN_PA06
+
+#endif
